bcc12261ce92012a89af926ecb047b212ec5cca3
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2019-08-03 12:21:36 -0300

    Passagem de parâmetros

--- src/main/java/ponto/WebDriverExample.java
+++ src/main/java/ponto/WebDriverExample.java
@@ -14,29 +14,34 @@ import java.util.*;
 
 public class WebDriverExample {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "D:\\Developer\\Tools\\Java\\geckodriver\\geckodriver.exe");
+		System.out.println("Passar na linha de comando: usuário senha data_inicio data fim");
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-
 		driver.get("https://app.pontomaisweb.com.br/");
-		driver.manage().window().setSize(new Dimension(1936, 1056));
-		driver.findElement(By.name("password")).sendKeys("senha");
-		driver.findElement(By.name("login")).sendKeys("usuario");
+		driver.manage().window().setSize(new Dimension(1936, 1056));		
+		System.out.println("Realizando Login");
+		driver.findElement(By.name("login")).sendKeys(args[0]);
+		driver.findElement(By.name("password")).sendKeys(args[1]);
 		driver.findElement(By.cssSelector(".btn-primary")).click();
 		try {
 			Thread.currentThread().sleep(10000);
+			System.out.println("Acessando lista do ponto");
 			driver.findElement(By.xpath("/html[1]/body[1]/div[4]/div[2]/div[1]/div[1]/nav[1]/div[1]/ul[1]/li[5]/a[2]"))
 					.click();
 			Thread.currentThread().sleep(5000);
-			driver.findElement(By.xpath("//input[@name='start_date']")).sendKeys("2019-07-01");
-			driver.findElement(By.xpath("//input[@name='end_date']")).sendKeys("2019-07-30");
+			System.out.println("Informando as datas");
+			driver.findElement(By.xpath("//input[@name='start_date']")).sendKeys(args[2]);
+			driver.findElement(By.xpath("//input[@name='end_date']")).sendKeys(args[3]);
+			System.out.println("Clicando no Filtro");
 			driver.findElement(
 					By.xpath("//div[@id=\'content-wrapper\']/div[2]/div/ng-view/div[2]/div/div/div/div[3]/button/i"))
 					.click();
 			Thread.currentThread().sleep(5000);
-
+			
 			List<WebElement> rows = driver.findElements(By.xpath(
 					"/html[1]/body[1]/div[4]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/ng-view[1]/div[2]/div[2]/table[1]/tbody/tr"));
-			System.out.println(rows.size());
+			System.out.println("Total de Regisstros:" + rows.size());
+			System.out.println("CApturando Dados");
 			for (int i = 1; i < rows.size() + 1; i++) {
 				WebElement diaElement = driver.findElement(By.xpath(
 						"/html[1]/body[1]/div[4]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/ng-view[1]/div[2]/div[2]/table[1]/tbody[1]/tr["
@@ -75,5 +80,7 @@ public class WebDriverExample {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Terminando o Programa");
+		driver.close();
 	}
 }
